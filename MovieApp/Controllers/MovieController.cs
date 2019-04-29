cf43f0f7d847e689d2858c7923a9a782a1a528b5
scorluka <stjepan22corluka@gmail.com>
2019-04-29 19:43:23 +0200

    added movie app

---
+++ MovieApp/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieApp.Controllers
+{
+    public class MovieController : Controller
+    {
+        MovieContext db = new MovieContext();
+
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View(db.Movies.ToList());
+        }
+    }
+}
